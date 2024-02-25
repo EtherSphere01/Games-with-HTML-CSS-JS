a44a2a9540d52b551a8aa362ea865e1c2e697a3e
Naimur Rahman <84018165+EtherSphere01@users.noreply.github.com>
2024-02-26 01:18:20 +0600

    update

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 let userScore = 0;
 let compScore = 0;
+let draw = 0;
 
 const choices = document.querySelectorAll(".choice");
+const reset = document.querySelector(".reset");
 const msg = document.querySelector("#msg");
 const userScorePara = document.querySelector("#user-score");
 const compScorePara = document.querySelector("#comp-score");
+const drawPara = document.querySelector("#draw");
+const userImageChange = document.querySelector(".p-change");
+const compImageChange = document.querySelector(".c-change");
 
 const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
   let num = Math.floor(Math.random() * 3);
+  compImageChange.src = `./images/${options[num]}.png`;
   return options[num];
 };
 
-const drawGame = () => {
+const drawGame = (draw) => {
   msg.innerText = "Game draw";
   msg.style.backgroundColor = "#081b31";
+  drawPara.innerText = draw;
 };
 
 const showWinner = (userWin, compChoice) => {
@@ -37,16 +44,21 @@ const playGame = (userChoice) => {
 
   // Draw
   if (userChoice === compChoice) {
-    drawGame();
+    draw++;
+    drawGame(draw);
+    userImageChange.src = `./images/${userChoice}.png`;
   } else {
     userWin = true;
 
     if (userChoice === "rock") {
       userWin = compChoice === "paper" ? false : true;
-    } else if ((userChoice = "paper")) {
+      userImageChange.src = `./images/${userChoice}.png`;
+    } else if (userChoice === "paper") {
       userWin = compChoice === "scissors" ? false : true;
+      userImageChange.src = `./images/${userChoice}.png`;
     } else {
       userWin = compChoice === "rock" ? false : true;
+      userImageChange.src = `./images/${userChoice}.png`;
     }
 
     showWinner(userWin, userChoice, compChoice);
@@ -58,4 +70,20 @@ choices.forEach((choice) => {
     const userChoice = choice.getAttribute("id");
     playGame(userChoice);
   });
+});
+
+reset.addEventListener("click", () => {
+  userScore = 0;
+  compScore = 0;
+  draw = 0;
+
+  userScorePara.innerText = "0";
+  compScorePara.innerText = "0";
+  drawPara.innerText = "0";
+
+  msg.innerText = "Play your move";
+  msg.style.backgroundColor = "#081b31";
+
+  userImageChange.src = "./images/user.png";
+  compImageChange.src = "./images/computer.png";
 });
